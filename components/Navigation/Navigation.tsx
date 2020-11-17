a0568480fa2e28f45297487f53a355b57801c4b8
Bartłomiej Mazur <ab.mazur@gmail.com>
2020-11-17 15:35:13 +0100

    Navigation component

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,11 +1,18 @@
+import Link from "next/link"
 import Logo from "../Logo/Logo"
 import styles from "./Navigation.module.scss"
 
 const Navigation = () => {
   return (
     <nav className={styles.navigation}>
-      <Logo width={500} height={200}/>
-      <div>sdfsdfsadf</div>
+      <Logo width={125} height={60}/>
+      <ul className={styles.navList}>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#clients">Clients</a></li>
+        <li><a href="#pricing">Pricing</a></li>
+      </ul>
+      <Link href=''><a className={styles.reviewBtn}>Get review</a></Link>
     </nav>
   );
 }
