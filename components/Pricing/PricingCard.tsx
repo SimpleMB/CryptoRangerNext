39d80efbc608e57cd25d4a11c3c7d648c3575b5f
Bartłomiej Mazur <ab.mazur@gmail.com>
2020-11-23 16:05:43 +0100

    Styling of PricingCard component

--- components/Pricing/PricingCard.tsx
+++ components/Pricing/PricingCard.tsx
@@ -12,10 +12,12 @@ const PricingCard: React.FC<PricingCardProps> = (props) => {
   const { type, priceCents, saleCents, language, flagUri } = props.data;
 
   return (
-    <li className={type === 'free' ? styles.freeCard : styles.paidCard}>
+    <li className={[type === 'free' ? styles.freeCard : styles.paidCard , styles.card].join(' ')}>
       <div className={styles.cardHeader}>
-        <Image src={flagUri} height={200} width={100} />
-        <h3>{type === 'free' ? 'Free' : language.toUpperCase()}</h3>
+        <h3>
+          <Image src={flagUri} height={50} width={100} />
+          {type === 'free' ? 'Free' : language.toUpperCase()}
+        </h3>
       </div>
       <h4 className={styles.cardPrice}>${priceCents / 100}</h4>
       <Link href="">
