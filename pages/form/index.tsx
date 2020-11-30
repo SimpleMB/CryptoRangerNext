ebc7e560e2a2ddeec16920ea31968ebd0cb004f4
Bartłomiej Mazur <ab.mazur@gmail.com>
2020-11-30 16:19:46 +0100

    Form component with list of Inputs created automatically based on fetched data

--- pages/form/index.tsx
+++ pages/form/index.tsx
@@ -2,46 +2,47 @@ import { useForm } from 'react-hook-form';
 import FormBigInput from '../../components/Form/FormBigInput';
 import FormSmallInput from '../../components/Form/FormSmallInput';
 
+enum InputType {
+  big = 'big',
+  small = 'small',
+  date = 'date',
+}
+
 interface InputProps {
+  name: string;
+  label: string;
+  value: string;
+  type: InputType;
+  rows?: number;
+  required?: boolean;
+}
+
+interface FormProps {
   projectName: string;
   projectWeb: string;
   whenProjectStarts: string;
   coreConcept: string;
 }
 
-const Form: React.FC<InputProps> = (props) => {
-  const { register, handleSubmit, watch, errors } = useForm<InputProps>();
-  const onSubmit = (data: InputProps) => console.log(data);
+interface FormProp {
+  form: InputProps[];
+}
+
+const Form: React.FC<FormProp> = ({ form }) => {
+  const { register, handleSubmit, watch, errors } = useForm<FormProp>();
+  const onSubmit = (data: FormProp) => console.log(data);
+
+  const inputList = form.map((input, index) => {
+    if (input.type === 'small')
+      return <FormSmallInput key={index} {...input} register={register} />;
+    if (input.type === 'big')
+      return <FormBigInput {...input} key={index} register={register} />;
+    if (input.type === 'date') return null;
+  });
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <FormSmallInput
-        type="text"
-        label="Project's name"
-        name="projectName"
-        register={register}
-        defaultValue={props.projectName}
-        required
-      />
-      <input
-        type="text"
-        name="projectWeb"
-        ref={register}
-        defaultValue={props.projectWeb}
-      />
-      <input
-        type="date"
-        name="whenProjectStarts"
-        ref={register}
-        defaultValue={props.whenProjectStarts}
-      />
-      <FormBigInput
-        label="Core concept"
-        name="coreConcept"
-        register={register}
-        defaultValue={props.coreConcept}
-        rows={5}
-      />
+      {inputList}
       <input type="submit" />
     </form>
   );
@@ -51,10 +52,41 @@ export async function getServerSideProps() {
   // Fetch initial data for the form props
   return {
     props: {
-      projectName: 'P name',
-      projectWeb: 'www',
-      whenProjectStarts: '10-10-10',
-      coreConcept: 'some shit...',
+      form: [
+        {
+          id: 0,
+          name: 'projectName',
+          label: "Project's Name",
+          value: 'P name',
+          type: 'small',
+          required: true,
+        },
+        {
+          id: 1,
+          name: 'projectWeb',
+          label: "Project's website",
+          value: 'www.awesomeproject.com',
+          type: 'small',
+          required: true,
+        },
+        {
+          id: 2,
+          name: 'projectStart',
+          label: 'When project starts',
+          value: '2020-12-01',
+          type: 'date',
+          required: true,
+        },
+        {
+          id: 3,
+          name: 'coreConcept',
+          label: 'Core concept of the project',
+          value: 'some legit shit...',
+          type: 'big',
+          rows: 3,
+          required: true,
+        },
+      ],
     },
   };
 }
