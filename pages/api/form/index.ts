bd833d80c9aa3582ecc0348609f0b0e3ccb70068
Bartłomiej Mazur <ab.mazur@gmail.com>
2020-12-02 19:25:11 +0100

    api/form route with dummy data

--- pages/api/form/index.ts
+++ pages/api/form/index.ts
@@ -1,1 +1,14 @@
 
+import { NextApiRequest, NextApiResponse } from 'next';
+import nc from 'next-connect';
+
+import formData from './formData.json';
+
+const handler = nc<NextApiRequest, NextApiResponse>();
+
+handler.get((req, res) => {
+  res.statusCode = 200;
+  res.json(formData);
+});
+
+export default handler;
