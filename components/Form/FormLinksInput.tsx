78a1a21fd7181959838221d90307365b404fb7ed
Bartłomiej Mazur <ab.mazur@gmail.com>
2020-12-03 14:57:45 +0100

    linting upgraded

--- components/Form/FormLinksInput.tsx
+++ components/Form/FormLinksInput.tsx
@@ -1,1 +1,7 @@
 // TODO: input with links
+
+const FormLinksInput: React.FC = () => {
+  return <div>Enter</div>;
+};
+
+export default FormLinksInput;
--- pages/form/index.tsx
+++ pages/form/index.tsx
@@ -28,11 +28,12 @@ const Form: React.FC<FormProps> = ({ formFields }) => {
   const { register, handleSubmit, watch, errors } = useForm<FormProps>();
   const onSubmit = (data: FormProps) => console.log(data);
 
-  const inputList = formFields.map((input, index) => {
+  const inputList = formFields.map((input) => {
     if (input.type === 'small' || input.type === 'date')
-      return <FormSmallInput key={index} {...input} register={register} />;
+      return <FormSmallInput key={input.id} {...input} register={register} />;
     if (input.type === 'big')
-      return <FormBigInput {...input} key={index} register={register} />;
+      return <FormBigInput {...input} key={input.id} register={register} />;
+    return null;
   });
 
   return (
@@ -53,191 +54,17 @@ const Form: React.FC<FormProps> = ({ formFields }) => {
 };
 
 export async function getServerSideProps() {
-  // Fetch initial data for the form props
   const res = await fetch('http://localhost:3000/api/form', {
-    method: 'GET', // *GET, POST, PUT, DELETE, etc.
+    method: 'GET',
     headers: {
       'Content-Type': 'application/json',
     },
+    body: null,
   });
   const data: FormProps = await res.json();
   return {
-    props: data
-  }
-
-  // return {
-  //   props: {
-  //     formFields: [
-  //       {
-  //         id: 'projectName',
-  //         name: 'projectName',
-  //         label: "Project's name:",
-  //         value: '',
-  //         type: 'small',
-  //         required: true,
-  //       },
-  //       {
-  //         id: 'projectWeb',
-  //         name: 'projectWeb',
-  //         label: "Project's website:",
-  //         value: '',
-  //         type: 'small',
-  //         required: true,
-  //       },
-  //       {
-  //         id: 'projectStart',
-  //         name: 'projectStart',
-  //         label: 'When project starts:',
-  //         value: '',
-  //         type: 'date',
-  //         required: true,
-  //       },
-  //       {
-  //         id: 'coreConcept',
-  //         name: 'coreConcept',
-  //         label: 'Core concept of the project:',
-  //         value: '',
-  //         type: 'big',
-  //         rows: 10,
-  //         required: false,
-  //       },
-  //       {
-  //         id: 'simpleAbout',
-  //         name: 'simpleAbout',
-  //         label: 'What is your project about? Explain it as simply as you can:',
-  //         value: '',
-  //         type: 'big',
-  //         rows: 4,
-  //         required: false,
-  //       },
-  //       {
-  //         id: 'useExample',
-  //         name: 'useExample',
-  //         label: 'Provide simple example of use:',
-  //         value: '',
-  //         type: 'big',
-  //         rows: 4,
-  //         required: false,
-  //       },
-  //       {
-  //         id: 'products',
-  //         name: 'products',
-  //         label: 'Main products:',
-  //         value: '',
-  //         type: 'big',
-  //         rows: 4,
-  //         required: false,
-  //       },
-  //       {
-  //         id: 'coreConcept',
-  //         name: 'coreConcept',
-  //         label: 'Core concept of the project:',
-  //         value: 'some legit shit...',
-  //         type: 'big',
-  //         rows: 4,
-  //         required: false,
-  //       },
-  //       {
-  //         id: 'benefits',
-  //         name: 'benefits',
-  //         label: 'What are the benefits of using your products:',
-  //         value: '',
-  //         type: 'big',
-  //         rows: 4,
-  //         required: false,
-  //       },
-  //       {
-  //         id: 'projectIdea',
-  //         name: 'projectIdea',
-  //         label: 'How did you came up with idea to start such project?',
-  //         value: '',
-  //         type: 'big',
-  //         rows: 4,
-  //         required: false,
-  //       },
-  //       {
-  //         id: 'importandToKnow',
-  //         name: 'importandToKnow',
-  //         label:
-  //           'What are the most importand things in your project that your future customer NEED to know?',
-  //         value: '',
-  //         type: 'big',
-  //         rows: 4,
-  //         required: false,
-  //       },
-  //       {
-  //         id: 'closestCompetitor',
-  //         name: 'closestCompetitor',
-  //         label: 'Who is your closest competitor?',
-  //         value: '',
-  //         type: 'big',
-  //         rows: 4,
-  //         required: false,
-  //       },
-  //       {
-  //         id: 'diffCompetitor',
-  //         name: 'diffCompetitor',
-  //         label:
-  //           "What's the difference between your project and closest competitor?",
-  //         value: '',
-  //         type: 'big',
-  //         rows: 4,
-  //         required: false,
-  //       },
-  //       {
-  //         id: 'advantageCompetitor',
-  //         name: 'advantageCompetitor',
-  //         label: 'Advantages over competition:',
-  //         value: '',
-  //         type: 'big',
-  //         rows: 4,
-  //         required: false,
-  //       },
-  //       {
-  //         id: 'futurePromotions',
-  //         name: 'futurePromotions',
-  //         label: 'Future promotions / sales / air drops?',
-  //         value: '',
-  //         type: 'big',
-  //         rows: 4,
-  //         required: false,
-  //       },
-  //       {
-  //         id: 'manyPeople',
-  //         name: 'manyPeople',
-  //         label: 'How many people work on this project?',
-  //         value: '',
-  //         type: 'small',
-  //         required: false,
-  //       },
-  //       {
-  //         id: 'aboutOwner',
-  //         name: 'aboutOwner',
-  //         label: 'About owner of the project?',
-  //         value: '',
-  //         type: 'big',
-  //         rows: 5,
-  //         required: false,
-  //       },
-  //       {
-  //         id: 'linksDesc',
-  //         name: 'linksDesc',
-  //         label: 'Importand links to place in the video description:',
-  //         value: '',
-  //         type: 'links',
-  //         required: false,
-  //       },
-  //       {
-  //         id: 'whenPublished',
-  //         name: 'whenPublished',
-  //         label: 'When review must be published?',
-  //         value: '2020-12-07',
-  //         type: 'date',
-  //         required: true,
-  //       },
-  //     ],
-    // },
-  // };
+    props: data,
+  };
 }
 
 export default Form;
