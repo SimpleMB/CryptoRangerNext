74f734b5c1692116923351a615cec6a14aaff73d
Bartłomiej Mazur <ab.mazur@gmail.com>
2020-12-18 15:01:44 +0100

    Proper session retrival on API/projects

--- pages/api/projects/index.ts
+++ pages/api/projects/index.ts
@@ -1,29 +1,15 @@
-// import { NextApiRequest, NextApiResponse } from 'next';
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getSession } from 'next-auth/client';
-// import nc from 'next-connect';
+import nc from 'next-connect';
 
-// const handler = nc<NextApiRequest, NextApiResponse>();
+const handler = nc<NextApiRequest, NextApiResponse>();
 
-// import { getSession } from 'next-auth/client'
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+handler.get(async (req: NextApiRequest, res: NextApiResponse) => {
   const session = await getSession({ req });
-  if (session) {
-    // Signed in
-    console.log('Session api: ', JSON.stringify(session, null, 2));
-  } else {
-    // Not Signed in
-    res.status(401);
-  }
+  console.log('session: ', session);
   res.json({ hello: 'siema' });
-  // res.end();
-};
-// handler.get(async (req: NextApiRequest, res: NextApiResponse) => {
-//   const session = await getSession({ req });
-//   console.log('session: ', session);
-// });
+});
 
-// export default handler;
+export default handler;
 
 // TODO: connect to Prisma and get user form with data
