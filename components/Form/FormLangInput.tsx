f57416ad09101ca16fc62183b952a0c9a99bc5ba
Bartłomiej Mazur <ab.mazur@gmail.com>
2021-01-14 20:59:36 +0100

    Prices update, LangInput component changed to radio, fixes

--- components/Form/FormLangInput.tsx
+++ components/Form/FormLangInput.tsx
@@ -1,5 +1,6 @@
 import { Input } from '../../types';
 import styles from './FormLangInput.module.scss';
+import prices from '../../utils/dummies/prices.json';
 
 type RefReturn =
   | string
@@ -30,29 +31,51 @@ const FormLangInput: React.FC<InputProps> = ({
   const isEnglish = value.includes('english');
   return (
     <fieldset className={styles.langInput}>
-      <label className={styles.langInputLabel} htmlFor={`${fieldName}English`}>
-        {label}
-      </label>
-      <input
-        id={`${fieldId}English`}
-        className={styles.langInputField}
-        type="checkbox"
-        name={`${fieldName}English`}
-        ref={register({ required })}
-        defaultValue="english"
-        // required={required}
-        defaultChecked={isEnglish}
-      />
+      <legend className={styles.langInputLabel}>{label}</legend>
+
       <input
         id={`${fieldId}Polish`}
         className={styles.langInputField}
-        type="checkbox"
-        name={`${fieldName}Polish`}
+        type="radio"
+        name={fieldName}
+        defaultValue="Polish"
         ref={register({ required })}
-        defaultValue="polish"
         // required={required}
         defaultChecked={isPolish}
       />
+      <label htmlFor={`${fieldId}Polish`}>
+        {`Polish - ${prices.polish.priceCents / 100}$`}
+      </label>
+
+      <input
+        id={`${fieldId}English`}
+        className={styles.langInputField}
+        type="radio"
+        name={fieldName}
+        defaultValue="English"
+        ref={register({ required })}
+        // required={required}
+        defaultChecked={isEnglish}
+      />
+      <label htmlFor={`${fieldId}English`}>
+        {`English - ${prices.english.priceCents / 100}$`}
+      </label>
+
+      <input
+        id={`${fieldId}Both`}
+        className={styles.langInputField}
+        type="radio"
+        name={fieldName}
+        defaultValue="English"
+        ref={register({ required })}
+        // required={required}
+        defaultChecked={isEnglish}
+      />
+      <label htmlFor={`${fieldId}Polish`}>
+        {`English AND Polish (2 separate reviews) - ${
+          prices.both.priceCents / 100
+        }$`}
+      </label>
     </fieldset>
   );
 };
