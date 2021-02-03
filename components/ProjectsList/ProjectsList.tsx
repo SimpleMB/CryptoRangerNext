35aca490b8f4a439460d8bb789229b909b28656a
Bartłomiej Mazur <ab.mazur@gmail.com>
2021-02-03 23:08:34 +0100

    Upgraded projects list

--- components/ProjectsList/ProjectsList.tsx
+++ components/ProjectsList/ProjectsList.tsx
@@ -1,15 +1,26 @@
 import { Project } from '../../types';
 import Card from '../Card/Card';
 import ListHeader from '../ProjectsListHeader/ProjectsListHeader';
+import styles from './ProjectsList.module.scss';
 
-const ProjectsList = ({ projects }) => {
+interface Props {
+  projects: Project[];
+}
+
+const ProjectsList: React.FC<Props> = ({ projects }) => {
   const projectsList = projects.map((project: Project) => (
     <Card key={project.id} project={project} />
   ));
   return (
     <>
       <ListHeader />
-      <ul>{projectsList}</ul>
+      {projects.length > 0 ? (
+        <ul>{projectsList}</ul>
+      ) : (
+        <p className={styles.projectListNothing}>
+          Nothing to show here... Create new project.
+        </p>
+      )}
     </>
   );
 };
