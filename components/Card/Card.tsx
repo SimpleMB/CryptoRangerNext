9141ae7e0ba33b79b55d99770d109726bf332486
Bartłomiej Mazur <ab.mazur@gmail.com>
2021-02-22 11:53:37 +0100

    Mobile styles for Card comp

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -30,11 +30,11 @@ const Card: React.FC<Props> = ({ project }) => {
       <a className={styles.cardLink}>
         <li className={styles.cardWrapper}>
           <div className={styles.cardId}>{id}</div>
-          <div>{created}</div>
+          <div className={styles.cardCreated}>{created}</div>
           <div className={styles.cardTitle}>{formFields[0].value}</div>
           <div className={styles.cardPublication}>{published ? '✔' : '❌'}</div>
-          <div className={styles.cardPublication}>{requested ? '✔' : '❌'}</div>
-          <div className={styles.cardPayment}>{payment}</div>
+          <div className={styles.cardRequested}>{requested ? '✔' : '❌'}</div>
+          <div className={styles.cardPaymentStatus}>{payment}</div>
           <div className={styles.cardUpdated}>{updated}</div>
         </li>
       </a>
