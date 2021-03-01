02b3e0335ec68e55bb1f55bbd653757309e77412
Bartłomiej Mazur <ab.mazur@gmail.com>
2021-03-01 19:27:09 +0100

    Forbiden component with better styles

--- components/Forbiden/Forbiden.tsx
+++ components/Forbiden/Forbiden.tsx
@@ -1,19 +1,26 @@
 import { signIn } from 'next-auth/client';
+import Navigation from '../Navigation/Navigation';
 import styles from './Forbiden.module.scss';
 
 const Forbiden = () => {
   return (
-    <div className={styles.forbidenWrapper}>
-      <h2>Hey there!</h2>
-      <h3> You don't have permision to view this content</h3>
-      <button
-        type="button"
-        className={styles.signInBtn}
-        onClick={() => signIn()}
-      >
-        Sign In!
-      </button>
-    </div>
+    <>
+      <Navigation />
+      <div className={styles.forbidenWrapper}>
+        <h3>Hey there!</h3>
+        <p>
+          Before you get access to your project's dashboard please click the
+          button below
+        </p>
+        <button
+          type="button"
+          className={styles.signInBtn}
+          onClick={() => signIn()}
+        >
+          Sign In!
+        </button>
+      </div>
+    </>
   );
 };
 
