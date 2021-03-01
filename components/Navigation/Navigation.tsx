c377649f1e1980c05b060f8ea24e3514d81e90d4
Bartłomiej Mazur <ab.mazur@gmail.com>
2021-03-01 19:28:20 +0100

    Navigation with extended and simple view

--- components/Navigation/Navigation.tsx
+++ components/Navigation/Navigation.tsx
@@ -1,7 +1,11 @@
 import Link from 'next/link';
 import styles from './Navigation.module.scss';
 
-const Navigation: React.FC = () => {
+interface Props {
+  extended?: boolean;
+}
+
+const Navigation: React.FC<Props> = ({ extended }) => {
   const scrollToId = (e: React.MouseEvent<HTMLButtonElement>) => {
     const destinationId = e.currentTarget.getAttribute('data-href');
     const desctinationElement = document.querySelector(destinationId);
@@ -9,44 +13,52 @@ const Navigation: React.FC = () => {
   };
   return (
     <nav className={styles.navigation}>
-      <img src="/images/cryptorangerlogo.svg" alt="Crypto Ranger logo sign" />
-      <ul className={styles.navList}>
-        <li>
-          <button type="button" data-href="#home" onClick={scrollToId}>
-            Home
-          </button>
-        </li>
-        <li>
-          <button
-            type="button"
-            data-href="#about"
-            onClick={scrollToId}
-            data-active
-          >
-            About
-          </button>
-        </li>
-        <li>
-          <button type="button" data-href="#clients" onClick={scrollToId}>
-            Clients
-          </button>
-        </li>
-        <li>
-          <button type="button" data-href="#pricing" onClick={scrollToId}>
-            Pricing
-          </button>
-        </li>
-      </ul>
-      <Link href="/projects">
+      <Link href="/">
+        <img src="/images/cryptorangerlogo.svg" alt="Crypto Ranger logo sign" />
+      </Link>
+      {extended && (
+        <ul className={styles.navList}>
+          <li>
+            <button type="button" data-href="#home" onClick={scrollToId}>
+              Home
+            </button>
+          </li>
+          <li>
+            <button
+              type="button"
+              data-href="#about"
+              onClick={scrollToId}
+              data-active
+            >
+              About
+            </button>
+          </li>
+          <li>
+            <button type="button" data-href="#clients" onClick={scrollToId}>
+              Clients
+            </button>
+          </li>
+          <li>
+            <button type="button" data-href="#pricing" onClick={scrollToId}>
+              Pricing
+            </button>
+          </li>
+        </ul>
+      )}
+      <button type="button" className={styles.reviewBtn}>
+        Login to dashboard
+      </button>
+      {/* <Link href="/projects">
         <a>
           <button type="button" className={styles.reviewBtn}>
             Get review
           </button>
         </a>
-      </Link>
+      </Link> */}
     </nav>
   );
 };
 
 export default Navigation;
 // TODO : animations when away from top
+// TODO: create new component with ul NavList
